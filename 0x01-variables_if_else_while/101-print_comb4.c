57359a1f58aa29a7b1c526def430b7a6ab763184
Hamzat Ibrahim . O <ibrokane76@gmail.com>
2022-09-12 22:27:46 -0700

    harder

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,19 +11,20 @@ int main(void)
 
 	for (i = 0; i < 8; i++)
 	{
-		for (k = j + 1; k < 10; k++)
+		for (j = i + 1; j < 9; j++)
 		{
-			putchar(i + '0');
-			putchar(j + '0');
-			putchar(k + '0');
-			if (i == 7 && j == 8 && k == 9)
-				break;
-			putchar(',');
-			putchar(' ');
+			for (k = j + 1; k < 10; k++)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+				if (i == 7 && j == 8 && k == 9)
+					break;
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
-}
-putchar('\n');
-
-return (0);
+	putchar('\n');
+	return (0);
 }
