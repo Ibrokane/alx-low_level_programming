a9d03ce2538d978ce8c81705e639753ea7adc29a
Hamzat Ibrahim . O <ibrokane76@gmail.com>
2022-09-12 21:45:37 -0700

    printing digit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - printing digit
+ *
+ * Return: alaways 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; < 10; i++)
+	{
+		putchar(i + '0');
+		if (1 == 9)
+			break;
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
