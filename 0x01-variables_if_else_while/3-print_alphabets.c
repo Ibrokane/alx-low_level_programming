f3e53a707f31aee45671aa2f699123052e3ceb53
Hamzat Ibrahim . O <ibrokane76@gmail.com>
2022-09-12 20:28:07 -0700

    lwer and upper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - upper and lower
+ *
+ * Return: aways 0
+ */
+ int main(void)
+{
+	char uplo[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 52; i++)
+		putchar(uplo[i]);
+	putchar('\n');
+	return (0);
+}
