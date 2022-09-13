592673093e40e6e73aa067ff727ac95254607bb4
Hamzat Ibrahim . O <ibrokane76@gmail.com>
2022-09-12 20:11:40 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - lowercase
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char lower[26] = "abcdefghijklmnopqrstuvwxyz';
+	int i;
+
+	for (i = 0; i < 26; i++)
+
+		putchar(lower[i]);
+	putchar('\n);
+	return (0);
+}
