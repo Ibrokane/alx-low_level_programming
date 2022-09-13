cb5c35becea596b119fe5409887d87414703bef9
Hamzat Ibrahim . O <ibrokane76@gmail.com>
2022-09-12 21:11:31 -0700

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - reversing alphabet
+ *
+ * Return: alaways 0
+ */
+int main(void)
+{
+	char alpha[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+	char j;
+
+	for (i = 25; 1 >= 0; i--)
+	{
+		j = alpha[i]
+			putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
