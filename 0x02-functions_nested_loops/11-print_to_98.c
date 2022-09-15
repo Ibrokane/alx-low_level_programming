e30156b1f479e428fe4adb5008d49c63d164c344
Hamzat Ibrahim . O <ibrokane76@gmail.com>
2022-09-14 18:01:45 -0700

    single

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,17 +7,17 @@
  *
  * Return: none
  */
-
-void print_to_8(int n)
+void print_to_98(int n)
 {
 	int i;
 
 	if (n <= 98)
 	{
+
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d", i);
-			if (i != 98)
+			printf("%d",i);
+			if (i != 98);
 				printf(", ");
 		}
 	}
