734ecb3387ca06eb3938b3a8d48823d916d75b79
Hamzat Ibrahim . O <ibrokane76@gmail.com>
2022-09-19 17:34:51 -0700

    reversing strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,40 @@
+#include "main.h"
+int _strlen(char *s);
+
+/**
+ * print_rev - reverse a string
+ *
+ * @s: sting to be reverved
+ *
+ * Return: none
+ */
+void print_rev(char *s)
+{
+	int i, n;
+
+	n = _strlen(s) - 1;
+
+	for (i = n; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
+
+
+
+/**
+ * _strlen - lenght of string
+ *
+ * @s: pointer variable
+ *
+ * Author: Hamzat Ibrahim
+ * Return: An integer
+ */
+int _strlen(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0; s[i]; i++)
+		count = count + 1;
+	return (count);
+}
