4b351aeeb470f623098275452f865c828a6542a2
Hamzat Ibrahim . O <ibrokane76@gmail.com>
2022-09-19 17:55:01 -0700

    print half o a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,45 @@
+#include "main.h"
+int _strlen(char *s);
+
+/**
+ *puts_half - print the last half of string
+ *
+ * @str: string to print
+ * Return: none
+ */
+void puts_half(char *str)
+{
+	int i, len;
+
+	if (_strlen(str) % 2 == 0)
+	{
+		len = _strlen(str) / 2;
+	}
+	else
+		len = _strlen(str) / 2 + 1;
+
+	for (i = len; i < _strlen(str); i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
+
+/**
+ * _strlen - lenght of string
+ *
+ * @s: pointer variable
+ *
+ * Author: Hamzat Ibrahim
+ * Return: An integer
+ */
+int _strlen(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0; s[i]; i++)
+		count = count + 1;
+	return (count);
+}
